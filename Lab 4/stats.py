191780ce3ce1322de3632ef46f43a6504472f1eb
ecall130 <55065732+ecall130@users.noreply.github.com>
2019-09-22 21:11:04 -0400

    Create stats.py

---
+++ Lab 4/stats.py
@@ -0,0 +1,11 @@
+from statistics import mean
+list = [34, 23, 54, 43, 53, 43, 99, 43, 74, 57, 82, 80, 17]
+print("The mean is ", mean(list))
+
+from statistics import median
+list = [34, 23, 54, 43, 53, 43, 99, 43, 74, 57, 82, 80, 17]
+print("The median is", median(list))
+
+from statistics import mode
+list = [34, 23, 54, 43, 53, 43, 99, 43, 74, 57, 82, 80, 17]
+print("The mode is", mode(list))
